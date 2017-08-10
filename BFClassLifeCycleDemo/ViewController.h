5d6191b76885965d5a5ee23d8fc290fd5f16f445
BFAlex <838318741@qq.com>
2017-08-10 17:03:44 +0800

    Initial Commit

---
+++ BFClassLifeCycleDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BFClassLifeCycleDemo
+//
+//  Created by Readboy_BFAlex on 2017/8/10.
+//  Copyright © 2017年 Readboy_BFAlex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
